576d02b6820899bd5b4f4e342ccf38284314b415
SilPeter <peteeexd@gmail.com>
2017-09-28 21:17:13 -0400

    homework 5
    question 1 driver, child and parent and output

---
+++ Homework 5/Question 1/child.java
@@ -0,0 +1,12 @@
+
+
+class child extends parent{
+    int childInt = parentInt;
+
+
+    public void printInt(){
+        System.out.print("Inside child: " );
+        System.out.println(childInt);
+    }
+
+}
---
+++ Homework 5/Question 1/driver.java
@@ -0,0 +1,14 @@
+//import parent.*;
+
+public class driver{
+    public static void main(String args[]){
+        parent james = new parent();
+        // james.printInt();
+
+        child jeff = new child();
+        jeff.printInt();
+
+
+    }
+
+}
